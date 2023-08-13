e0e7a033814506425fc79b52f1a08b3c58e879ad
Ospina <o.lamprea99@gmail.com>
2023-08-12 23:10:59 -0500

    diccionario y lista de diccionarios creada

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import statistics
 
 
@@ -13,4 +12,17 @@ class Student:
 
     def average_grade(self):
         return statistics.mean(self.grades)
+
+
+lista: list[dict]
+lista_objetos: list[Student]
+
+
+def agregar_lista(diccionario: dict):
+    lista.append(diccionario)
+
+
+def diccionario_crear(estudiante: Student):
+    diccionario: dict = {estudiante.name: {estudiante.age, estudiante.grades}}
+    agregar_lista(diccionario)
 
