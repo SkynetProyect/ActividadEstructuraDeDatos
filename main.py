451d8e9fd46f4fc9c0c7e0fa5039c78e90c09105
Ospina <o.lamprea99@gmail.com>
2023-08-13 11:41:12 -0500

    Hasta el punto 7 realizado

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class Student:
         return statistics.mean(self.grades)
 
 
-lista: list[dict]
+lista: list[dict] = []
 lista_objetos: list[Student]
 
 
@@ -23,6 +23,26 @@ def agregar_lista(diccionario: dict):
 
 
 def diccionario_crear(estudiante: Student):
-    diccionario: dict = {estudiante.name: {estudiante.age, estudiante.grades}}
+    diccionario: dict = {estudiante.name: [estudiante.age, estudiante.grades]}
     agregar_lista(diccionario)
 
+
+def crear_lista_objetos(lista_e: list):  # recibe la lista de diccionarios y crea una lista de objetos
+    nombre: str = ""
+    edad: int = 0
+
+    for i in lista_e:
+        for j in lista_e:
+            nombre = lista_e[i][j]
+            edad = lista_e[i][j][0]
+        objeto: Student = Student(nombre, edad)
+        lista_objetos.append(objeto)
+
+
+def filtrar_estudiantes(lista_t: list, umbral: float):  # recibe una lista de objetos y los filtra en otra lista
+    umbral = umbral
+    lista_r: list = []
+    for i in lista_t:
+        if i.average_grade() >= umbral:
+            lista_r.append(i)
+    return lista_r
