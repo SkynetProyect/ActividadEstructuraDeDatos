885d649d21b31780a6e9fec8a9865e3910e2b1a0
Ospina <o.lamprea99@gmail.com>
2023-08-12 22:50:48 -0500

    Repositorio creado.

---
+++ main.py
@@ -0,0 +1,16 @@
+
+import statistics
+
+
+class Student:
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+        self.grades: list[int] = []
+
+    def add_grade(self, numero: int):
+        self.grades.append(numero)
+
+    def average_grade(self):
+        return statistics.mean(self.grades)
+
