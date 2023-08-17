f3764161a2eabb7ed77086ebc5aa609ca14f0093
SkynetProject <o.lamprea99gmail.com>
2023-08-17 13:31:40 -0500

    Tasks

--- funciones.py
+++ funciones.py
@@ -34,3 +34,10 @@ def diccionario_promedio(_lista: list, promedio: float):
     _filtrado = filtro_nota_nombre(_lista, promedio)
     diccionario_filtrado = {i.name: i for i in _filtrado}
     return diccionario_filtrado
+
+""" Puntos a realizar
+
+Generar llamadas y entradas para poder testear las funciones
+Asegurar que los for si retornen claves y valores en vez de diccionarios
+Hacer pruebas de funcionamiento con la info
+"""
